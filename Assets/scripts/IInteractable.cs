6736df7f1f757fca649e4ed5fbf00ad5ad9a1525
JoeyHogenson <106626656+JoeyHogenson@users.noreply.github.com>
2025-10-29 23:23:12 -0800

    Thinking emoji

--- Assets/scripts/IInteractable.cs
+++ Assets/scripts/IInteractable.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using Unity.EditorCoroutines.Editor;
+//using Unity.EditorCoroutines.Editor;
 public interface IInteractable
 {
     void Interact(); // prefom action when the player interacts with the object
