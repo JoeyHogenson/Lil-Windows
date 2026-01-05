70cd11de04f7108a26edee5086f6942bff24238c
cptcrabcakes <dylan.d.j.f@gmail.com>
2026-01-05 01:15:16 -0900

    Revert "actual"
    This reverts commit 46b6b4c02730e373378234b98d1fcd42b49a20cf.

--- Assets/scripts/PlayerLogic.cs
+++ Assets/scripts/PlayerLogic.cs
@@ -1,232 +1,344 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections;
 using TMPro;
 
 namespace StarterAssets
 {
-public class PlayerLogic : MonoBehaviour
-{
-    public int socialCred;
-    public int troubleMeter;
-    public int mentalHealth;
-
-    public GameObject eToTalkButton;
-    public GameObject eToOpenButton;
-    public GameObject eToCloseButton;
-    public GameObject eToGrabButton;
-    public GameObject menuPanel;
-
-    public GameObject[] Newspaper;
-    public int count;
-
-    public bool isManualOpen;
-    private string[] ManualText = 
-    {"Page 0 text",
-    "Welcome to Lil' Windows! \n\nYour Current Quests are: Talk to OG about needing medication\n\nExplore media materials in law library",
-    "Come back when you have completed your quests",
-    "","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
-
-    public TextMeshProUGUI leftPage;
-    public TextMeshProUGUI rightPage;
-
-    private int leftCount;
-    public TextMeshProUGUI leftPageNumber;
-    private int rightCount;
-    public TextMeshProUGUI rightPageNumber;
-
-    public bool startDialogue;
-
-    public string typeInteract;
-
-    private StarterAssetsInputs _input;
-    private PlayerInput _playerInput;
-
-    Collider collider;
-
-    public string currentEvent;
-
-    public Ray ray;
-    public RaycastHit hit;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+       public class PlayerLogic : MonoBehaviour
     {
-        
-        //setting _input variable to starter assets script to get inputs
-        startDialogue = true;
-        _input = GetComponent<StarterAssetsInputs>();
-        _playerInput = GetComponent<PlayerInput>();
-        isManualOpen = false;
-        count = 0;
-        leftCount = 1;
-        rightCount = 2;
-        Debug.Log(ManualText.Length);
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        _input.cursorInputForLook = true;
-        _input.cursorLocked = true;
-        
-    }
+        public int socialCred;
+        public int troubleMeter;
+        public int mentalHealth;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //shoot raycast every frame
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
-        //handles when raycast hits something
-        if(Physics.Raycast(ray, out hit))
+        public GameObject eToTalkButton;
+        public GameObject eToOpenButton;
+        public GameObject eToCloseButton;
+        public GameObject eToGrabButton;
+        public GameObject menuPanel;
+
+        public GameObject[] Newspaper;
+        public int count;
+
+        public bool isManualOpen;
+        private string[] ManualText =
         {
-            if(hit.distance <= 15f)
-            {
-                
-                collider = hit.collider; 
-                if(collider.GetComponent<Dialogue>())
-                {
-                    typeInteract = "Dialogue";
-                }
-                else if(collider.GetComponent<InteractTele>())
-                {
-                    typeInteract = "Tele";
-                    eToOpenButton.SetActive(true);
-                }
-                else if(collider.GetComponent<SimpleDoor>())
-                {
-                    if(!collider.GetComponent<SimpleDoor>().isOpen)
-                    {
-                        typeInteract = "Door";
-                        eToOpenButton.SetActive(true);
-                    }
-                    else
-                    {
-                        typeInteract = "Door";
-                        eToCloseButton.SetActive(true);
-                    }
+            "Page 0 text",
+            "Welcome to Lil' Windows! \n\nYour Current Quests are: Talk to OG about needing medication\n\nExplore media materials in law library",
+            "Come back when you have completed your quests",
+            "","","","","","","","","","","","","","","","","","","","","","","","","","","","",""
+        };
 
-                }
-                else if (collider.CompareTag("Newspaper"))
-                {
-                    typeInteract = "Newspaper";
-                    Newspaper[count].SetActive(true);
-                    count++;
-                }
-                else
-                {
-                    typeInteract = "None";
-                    eToCloseButton.SetActive(false);
-                    eToOpenButton.SetActive(false);
-                    
-                }
+        public TextMeshProUGUI leftPage;
+        public TextMeshProUGUI rightPage;
+
+        private int leftCount;
+        public TextMeshProUGUI leftPageNumber;
+        private int rightCount;
+        public TextMeshProUGUI rightPageNumber;
+
+        public bool startDialogue;
+
+        private enum InteractType { None, Dialogue, Tele, Door, Newspaper, Grabbable }
+        private InteractType currentInteract = InteractType.None;
+
+        private StarterAssetsInputs _input;
+        private PlayerInput _playerInput;
+        private FirstPersonController _firstPersonController;
+        private Camera _mainCamera;
+
+        private Collider currentCollider;
+
+        public string currentEvent;
+
+        private Ray ray;
+        private RaycastHit hit;
+
+        private const float InteractionMaxDistance = 15f;
+
+        void Awake()
+        {
+            _input = GetComponent<StarterAssetsInputs>();
+            _playerInput = GetComponent<PlayerInput>();
+            _firstPersonController = GetComponent<FirstPersonController>();
+        }
+
+        void Start()
+        {
+            startDialogue = true;
+            isManualOpen = false;
+            count = 0;
+            leftCount = 1;
+            rightCount = 2;
+
+            _mainCamera = Camera.main;
+            if (_mainCamera == null)
+            {
+                Debug.LogWarning("Main Camera not found. Raycasts will use Camera.main dynamically.");
+            }
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (_input != null)
+            {
+                _input.cursorInputForLook = true;
+                _input.cursorLocked = true;
+            }
+
+            Debug.Log($"ManualText length: {ManualText.Length}");
+        }
+
+        void Update()
+        {
+            DoInteractionRaycast();
+        }
+
+        private void DoInteractionRaycast()
+        {
+            // Choose cached camera if available
+            var cam = _mainCamera ?? Camera.main;
+            if (cam == null) return;
+
+            ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f));
+            if (Physics.Raycast(ray, out hit, InteractionMaxDistance))
+            {
+                currentCollider = hit.collider;
+                UpdateInteractStateFromCollider(currentCollider);
             }
             else
             {
-                typeInteract = "None";
-                eToCloseButton.SetActive(false);
-                eToOpenButton.SetActive(false);
+                currentCollider = null;
+                SetInteractNone();
             }
-            if(!hit.collider.CompareTag("NPC"))
+
+            // If the thing hit isn't an NPC, ensure talk button is hidden
+            if (currentCollider == null || !currentCollider.CompareTag("NPC"))
             {
-                eToTalkButton.SetActive(false);
+                eToTalkButton?.SetActive(false);
             }
         }
-        
-    }
-    public void Option1()
-    {
-        collider.GetComponent<DialogueController>().nextLineOption1();
-    }
-    /*
-    public void InitiateDialougue()
-    {
-        dialoguePanel.SetActive(true);
-        GetComponent<FirstPersonController>().MoveSpeed = 0f;
-        _input.cursorInputForLook = false;
-        hit.collider.gameObject.GetComponent<DialogueController>().onClick();
-    }
-    */
-    public void Interact()
-    {
-        //handles starting dialogue when you press "E". Reads off of the NPC dialogue scriptable object
-        if(typeInteract == "Dialogue" && startDialogue == true)
+
+        private void UpdateInteractStateFromCollider(Collider col)
         {
-            eToTalkButton.SetActive(false);         
-            hit.collider.GetComponent<Animator>().SetBool("isWalking", false);
-            hit.collider.GetComponent<Animator>().SetBool("sitTalkRight", true);
-            hit.collider.GetComponent<NPCController>().NPCspeed = 0f;
-            //GetComponent<FirstPersonController>().MoveSpeed = 0f;
-            //_input.cursorInputForLook = false;
-            _input.cursorLocked = true;
+            // Hide default open/close until determined
+            eToOpenButton?.SetActive(false);
+            eToCloseButton?.SetActive(false);
+            eToGrabButton?.SetActive(false);
+
+            if (col == null)
+            {
+                SetInteractNone();
+                return;
+            }
+
+            // Dialogue
+            if (col.TryGetComponent<Dialogue>(out _))
+            {
+                currentInteract = InteractType.Dialogue;
+                eToTalkButton?.SetActive(true);
+                return;
+            }
+
+            // Teleport interaction
+            if (col.TryGetComponent<InteractTele>(out _))
+            {
+                currentInteract = InteractType.Tele;
+                eToOpenButton?.SetActive(true);
+                return;
+            }
+
+            // Door
+            if (col.TryGetComponent<SimpleDoor>(out var door))
+            {
+                currentInteract = InteractType.Door;
+                if (!door.isOpen) eToOpenButton?.SetActive(true);
+                else eToCloseButton?.SetActive(true);
+                return;
+            }
+
+            // Newspaper by tag
+            if (col.CompareTag("Newspaper"))
+            {
+                currentInteract = InteractType.Newspaper;
+                ShowNextNewspaper();
+                return;
+            }
+
+            // Grabbable - tag or component can be adapted
+            if (col.CompareTag("Grabbable") || col.TryGetComponent<Rigidbody>(out _))
+            {
+                currentInteract = InteractType.Grabbable;
+                eToGrabButton?.SetActive(true);
+                return;
+            }
+
+            SetInteractNone();
+        }
+
+        private void SetInteractNone()
+        {
+            currentInteract = InteractType.None;
+            eToCloseButton?.SetActive(false);
+            eToOpenButton?.SetActive(false);
+            eToGrabButton?.SetActive(false);
+        }
+
+        public void Option1()
+        {
+            if (currentCollider == null) return;
+            if (currentCollider.TryGetComponent<DialogueController>(out var dc))
+            {
+                dc.nextLineOption1();
+            }
+        }
+
+        public void Interact()
+        {
+            if (currentCollider == null) return;
+
+            switch (currentInteract)
+            {
+                case InteractType.Dialogue:
+                    HandleDialogueInteract();
+                    break;
+                case InteractType.Door:
+                    HandleDoorInteract();
+                    break;
+                case InteractType.Tele:
+                    HandleTeleInteract();
+                    break;
+                case InteractType.Grabbable:
+                    eToGrabButton?.SetActive(true);
+                    break;
+                case InteractType.Newspaper:
+                    // Newspaper display handled when raycast discovered it.
+                    break;
+                case InteractType.None:
+                default:
+                    break;
+            }
+        }
+
+        private void HandleDialogueInteract()
+        {
+            if (!startDialogue)
+            {
+                if (currentCollider.TryGetComponent<DialogueController>(out var dc))
+                {
+                    dc.nextLine();
+                }
+                return;
+            }
+
+            // Start dialogue
+            eToTalkButton?.SetActive(false);
+
+            if (currentCollider.TryGetComponent<Animator>(out var animator))
+            {
+                animator.SetBool("isWalking", false);
+                animator.SetBool("sitTalkRight", true);
+            }
+
+            if (currentCollider.TryGetComponent<NPCController>(out var npc))
+            {
+                npc.NPCspeed = 0f;
+            }
+
+            if (_firstPersonController != null) _firstPersonController.MoveSpeed = 0f;
+            if (_input != null)
+            {
+                _input.cursorInputForLook = false;
+                _input.cursorLocked = true;
+            }
+
             startDialogue = false;
             Cursor.visible = true;
-            // Unlock the cursor so it can move freely
-            Cursor.lockState = CursorLockMode.None; 
+            Cursor.lockState = CursorLockMode.None;
         }
-        else if(typeInteract == "Dialogue" && startDialogue == false)
+
+        private void HandleDoorInteract()
         {
-            hit.collider.gameObject.GetComponent<DialogueController>().nextLine();
+            if (currentCollider.TryGetComponent<SimpleDoor>(out var door))
+            {
+                if (!door.isOpen)
+                {
+                    door.Open();
+                    eToOpenButton?.SetActive(false);
+                }
+                else
+                {
+                    door.Close();
+                    eToCloseButton?.SetActive(false);
+                }
+            }
         }
-        else if(typeInteract == "Door" && !hit.collider.GetComponent<SimpleDoor>().isOpen)
+
+        private void HandleTeleInteract()
         {
-            hit.collider.GetComponent<SimpleDoor>().Open();
-            eToOpenButton.SetActive(false);
+            if (currentCollider.TryGetComponent<InteractTele>(out var tele) && tele.targetLocation != null)
+            {
+                transform.position = tele.targetLocation.transform.position;
+            }
         }
-        else if(typeInteract == "Door" && hit.collider.GetComponent<SimpleDoor>().isOpen)
+
+        public void Menu()
         {
-            hit.collider.GetComponent<SimpleDoor>().Close();
-            eToCloseButton.SetActive(false);
-        }
-        else if(typeInteract == "grabbable")
-        {
-            eToGrabButton.SetActive(true);
-        }
-        else if(typeInteract == "Tele")
-        {
-                //teleport player to target location
-                transform.position = hit.collider.GetComponentInParent<InteractTele>().targetLocation.transform.position;
+            // If in dialogue, end dialogue and restore controls
+            if (currentInteract == InteractType.Dialogue && !startDialogue)
+            {
+                if (_firstPersonController != null) _firstPersonController.MoveSpeed = 10f;
+                if (_input != null)
+                {
+                    _input.cursorInputForLook = true;
+                    _input.cursorLocked = true;
+                }
+
+                eToTalkButton?.SetActive(false);
+                startDialogue = true;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                return;
             }
 
+            // If a newspaper is active, hide the last opened one
+            if (count > 0 && count <= Newspaper.Length && Newspaper[count - 1].activeSelf)
+            {
+                Newspaper[count - 1].SetActive(false);
+                return;
+            }
 
+            // Toggle menu panel
+            if (menuPanel == null) return;
 
+            var isActive = menuPanel.activeSelf;
+            menuPanel.SetActive(!isActive);
 
-        }
-    public void Menu()
-    {
-        //Ends dialogue; handles if esc is pressed when dialogue is active 
-        if(typeInteract == "Dialogue")
-        {
-            GetComponent<FirstPersonController>().MoveSpeed = 10f;
-            //hit.collider.GetComponent<NPCController>().NPCspeed = 0.03f;
-            //hit.collider.GetComponent<Animator>().SetBool("isWalking", true);
-            _input.cursorInputForLook = true;
-            _input.cursorLocked = true;
-            eToTalkButton.SetActive(false);
-            startDialogue = true;
-             Cursor.visible = false;
-            // Unlock the cursor so it can move freely
-            Cursor.lockState = CursorLockMode.Locked; 
-        }
-        else if(Newspaper[count].activeSelf == true)
-        {
-            Newspaper[count].SetActive(false);
+            if (_firstPersonController != null)
+            {
+                _firstPersonController.MoveSpeed = isActive ? 10f : 0f;
+            }
 
+            if (_input != null)
+            {
+                _input.cursorInputForLook = isActive;
+            }
         }
-        //Brings up Menu
-        else if(menuPanel.activeSelf == false)
+
+        private void ShowNextNewspaper()
         {
-            menuPanel.SetActive(true);
-            GetComponent<FirstPersonController>().MoveSpeed = 0f;
-            _input.cursorInputForLook = false;
-        }
-        else if(menuPanel.activeSelf == true)
-        {
-            menuPanel.SetActive(false);
-            GetComponent<FirstPersonController>().MoveSpeed = 10f;
-            _input.cursorInputForLook = true;
+            if (Newspaper == null || Newspaper.Length == 0) return;
+
+            // Ensure count is within bounds
+            if (count < 0) count = 0;
+            if (count >= Newspaper.Length) count = Newspaper.Length - 1;
+
+            // Activate the current newspaper if not already active
+            if (!Newspaper[count].activeSelf)
+            {
+                Newspaper[count].SetActive(true);
+            }
+
+            // increment for next time, but clamp
+            count = Mathf.Clamp(count + 1, 0, Newspaper.Length);
         }
     }
-    
-    
-
-}
 }
