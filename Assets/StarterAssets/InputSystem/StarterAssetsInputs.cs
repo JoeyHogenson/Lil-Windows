fd7a90523bc9e516dcabed94e665cf6ece0ff159
JoeyHogenson <106626656+JoeyHogenson@users.noreply.github.com>
2026-09-22 14:00:59 -0800

    How Now Brown Cow

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -94,6 +94,7 @@ namespace StarterAssets
 		private void SetCursorState(bool newState)
 		{
 			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+			Cursor.visible = !newState;
 		}
 	}
 	
--- Assets/scripts/PlayerLogic.cs
+++ Assets/scripts/PlayerLogic.cs
@@ -43,7 +43,7 @@ public class PlayerLogic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.visible = false;
         //setting _input variable to starter assets script to get inputs
         startDialogue = true;
         _input = GetComponent<StarterAssetsInputs>();
